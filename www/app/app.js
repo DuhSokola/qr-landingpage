476bedc8560b10d39e3b0ea699ac34e2532e4f0c
Dmitry Prudnikov <dmitry.prudnikov@amag.ch>
2016-01-05 15:20:42 +0100

    impl errors

--- www/app/app.js
+++ www/app/app.js
@@ -1,4 +1,4 @@
-;(function() {
+;(function () {
     'use strict';
 
     var deps = [
@@ -14,27 +14,79 @@
         $translateProvider.useSanitizeValueStrategy('sanitizeParameters');
 
         $translateProvider.translations('DE', {
-            "TITLE"                     : "CH",
-            "CONTACT_LABEL_FIRSTNAME"   : "Vorname",
-            "CONTACT_LABEL_LASTNAME"    : "Name",
-            "CONTACT_LABEL_STREET"      : "Strasse",
-            "CONTACT_LABEL_STREET_NR"   : "Strassen Nr.",
-            "CONTACT_LABEL_ZIP"         : "PLZ",
-            "CONTACT_LABEL_CITY"        : "Ort",
-            "CONTACT_LABEL_TELEPHONE"   : "Telefon",
-            "CONTACT_LABEL_EMAIL"       : "Email"
+            "TITLE": "CH",
+            "CONTACT_LEGEND_CUSTOMER_DATA": "Ihre Daten",
+            "CONTACT_LEGEND_CATALOG": "Katalogversand",
+            "CONTACT_LEGEND_TESTDRIVE": "Probefahrt",
+            "CONTACT_LEGEND_LEASING": "Leasing",
+            "CONTACT_LEGEND_NEWSLETTER": "Newsletter",
+            "CONTACT_LEGEND_PRIVACY": "Datenschutzbestimmung",
+            "CONTACT_MODELS": "Weitere Modelle",
+            "CONTACT_SALUTATION_MR": "Herr",
+            "CONTACT_SALUTATION_MRS": "Frau",
+            "CONTACT_SALUTATION": "Anrede",
+            "CONTACT_FIRSTNAME": "Vorname",
+            "CONTACT_LASTNAME": "Name",
+            "CONTACT_STREET": "Strasse",
+            "CONTACT_STREET_NR": "Strassen Nr.",
+            "CONTACT_ZIP": "PLZ",
+            "CONTACT_CITY": "Ort",
+            "CONTACT_TELEPHONE": "Telefon",
+            "CONTACT_EMAIL": "Email",
+            "CONTACT_CATALOG": "Katalog bestellen",
+            "CONTACT_CATALOG_ELECTRONIC": "Elektronisch",
+            "CONTACT_CATALOG_PRINTED": "Gedruckt",
+            "CONTACT_TESTDRIVE": "Kontaktaufnahme für Probefahrt",
+            "CONTACT_TESTDRIVE_EMAIL": "per Email",
+            "CONTACT_TESTDRIVE_TELEFON": "per Telefon",
+            "CONTACT_LEASING": "Kontaktaufnahme für Leasingsberatung",
+            "CONTACT_LEASING_EMAIL": "per Email",
+            "CONTACT_LEASING_TELEPHONE": "per Telefon",
+            "CONTACT_NEWSLETTER": "Ich möchte Newsletter erhalten",
+            "CONTACT_PRIVACY": "Ich bin mit den Datenschutzbestimmungen einverstanden",
+            "CONTACT_BUTTON_SUBMIT": "Senden",
+            "CONTACT_BUTTON_BACK": "Zurück",
+            "CONTACT_ERROR_SALUTATION": "Anrede pflicht",
+            "CONTACT_ERROR_FIRSTNAME": "Vorname pflicht",
+            "CONTACT_ERROR_LASTNAME": "Name pflicht",
+            "CONTACT_ERROR_STREET": "Strasse pflicht",
+            "CONTACT_ERROR_STREET_NR": "Strassen Nummer pflicht",
+            "CONTACT_ERROR_ZIP": "PLZ pflicht",
+            "CONTACT_ERROR_CITY": "Ort pflicht",
+            "CONTACT_ERROR_TELEPHONE": "Telefon pflicht",
+            "CONTACT_ERROR_EMAIL": "Email pflicht",
+            "CONTACT_ERROR_CATALOG": "Katalog pflicht",
+            "CONTACT_ERROR_TESTDRIVE": "Probefahrt pflicht",
+            "CONTACT_ERROR_LEASING": "Leasing pflicht",
+            "CONTACT_ERROR_NEWSLETTER": "Newsletter pflicht",
+            "CONTACT_ERROR_PRIVACY": "Datenschutzbestimmung pflicht"
         });
 
         $translateProvider.translations('FR', {
-            "TITLE"                     : "[FR]CH",
-            "CONTACT_LABEL_FIRSTNAME"   : "[FR]Vorname",
-            "CONTACT_LABEL_LASTNAME"    : "[FR]Name",
-            "CONTACT_LABEL_STREET"      : "[FR]Strassen Nr.",
-            "CONTACT_LABEL_STREET_NR"   : "[FR]Nr.",
-            "CONTACT_LABEL_ZIP"         : "[FR]PLZ",
-            "CONTACT_LABEL_CITY"        : "[FR]Ort",
-            "CONTACT_LABEL_TELEPHONE"   : "[FR]Telefon",
-            "CONTACT_LABEL_EMAIL"       : "[FR]Email"
+            "TITLE": "[FR]CH",
+            "CONTACT_SALUTATION_MR": "[FR]Herr",
+            "CONTACT_SALUTATION_MRS": "[FR]Frau",
+            "CONTACT_SALUTATION": "[FR]Anrede",
+            "CONTACT_FIRSTNAME": "[FR]Vorname",
+            "CONTACT_LASTNAME": "[FR]Name",
+            "CONTACT_STREET": "[FR]Strassen Nr.",
+            "CONTACT_STREET_NR": "[FR]Nr.",
+            "CONTACT_ZIP": "[FR]PLZ",
+            "CONTACT_CITY": "[FR]Ort",
+            "CONTACT_TELEPHONE": "[FR]Telefon",
+            "CONTACT_EMAIL": "[FR]Email",
+            "CONTACT_CATALOG": "[FR]Ich möchte ein Katalog bestellen",
+            "CONTACT_CATALOG_ELECTRONIC": "[FR]Elektronisch",
+            "CONTACT_CATALOG_PRINTED": "[FR]Gedruckt",
+            "CONTACT_TESTDRIVE": "[FR]Ich möchte eine Probefahrt bestellen",
+            "CONTACT_TESTDRIVE_EMAIL": "[FR]per Email",
+            "CONTACT_TESTDRIVE_TELEFON": "[FR]per Telefon",
+            "CONTACT_LEASING": "[FR]Ich möchte eine Leasingsberatung bekommen",
+            "CONTACT_LEASING_EMAIL": "[FR]per Email",
+            "CONTACT_LEASING_TELEFON": "[FR]per Telefon",
+            "CONTACT_NEWSLETTER": "[FR]Ich möchte Newsletter erhalten",
+            "CONTACT_PRIVACY": "[FR]Ich bin mit den Datenschutzbestimmungen einverstanden",
+            "CONTACT_BUTTON": "[FR]Senden"
         });
 
         $translateProvider.preferredLanguage('DE');
@@ -42,9 +94,15 @@
 
     app.run(function ($rootScope) {
         $rootScope.global = {};
-        $rootScope.global.languages = ['DE','FR','IT','EN'];
+        $rootScope.global.languages = ['DE', 'FR', 'IT', 'EN'];
 
         $rootScope.global.data = {};
     });
+
+
+    app.controller('testCtrl',['$scope', function ($scope) {
+        $scope.projectForm = {};
+        $scope.projectForm.clientEmail;
+    }]);
 
 }());
--- www/app/components/contactForm/contactForm.ctrl.js
+++ www/app/components/contactForm/contactForm.ctrl.js
@@ -1,12 +1,23 @@
 ;(function () {
     'use strict';
 
-    var dependencies = [
-    ];
+    var dependencies = [];
 
     var app = angular.module('app.contactForm.ctrl', dependencies);
 
-    app.controller('ContactFormCtrl', ['$scope', function ($scope) {
+    app.controller('ContactFormCtrl', ['$rootScope', '$scope', '$translate', function ($rootScope, $scope, $translate) {
+
+        $scope.salutations = [
+            {
+                translation: 'CONTACT_SALUTATION_MR',
+                value: 'MR'
+            }
+            , {
+                translation: 'CONTACT_SALUTATION_MRS',
+                value: 'MRS'
+            }];
+
+        $scope.data = {};
         $scope.data.salutation = '';
         $scope.data.firstName = '';
         $scope.data.lastName = '';
@@ -16,11 +27,221 @@
         $scope.data.city = '';
         $scope.data.telephone = '';
         $scope.data.email = '';
-        $scope.data.catalogElectronic = '';
+        $scope.data.catalog = '';
+        $scope.data.catalogTyp = '';
         $scope.data.testdrive = '';
+        $scope.data.testdriveTyp = '';
         $scope.data.leasing = '';
+        $scope.data.leasingTyp = '';
         $scope.data.privacyAccepted = '';
-        $scope.data.newsletter = '';
+        $scope.data.newsletter = true;
+
+        var setErrorChkbox = function (id) {
+            id = '#' + id;
+            $(id).removeClass('md-primary');
+            $(id).addClass('error-checkbox');
+            $(id).addClass('error-text');
+        };
+
+        var setNormalChkbox = function (id) {
+            id = '#' + id;
+            $(id).removeClass('error-text');
+            $(id).removeClass('error-checkbox');
+            $(id).addClass('md-primary');
+        };
+
+        var setErrorRadio = function (name) {
+            $('md-radio-button[name='+name+']').removeClass('md-primary');
+            $('md-radio-button[name='+name+']').addClass('error-radio');
+            $('md-radio-button[name='+name+']').addClass('error-text');
+        };
+
+        var setNormalRadio = function (name) {
+            $('md-radio-button[name='+name+']').removeClass('error-text');
+            $('md-radio-button[name='+name+']').removeClass('error-radio');
+            $('md-radio-button[name='+name+']').addClass('md-primary');
+        };
+
+        $scope.validate = function () {
+            var isValid = true;
+            if (!$scope.data.catalog && !$scope.data.testdrive && !$scope.data.leasing) {
+                setErrorChkbox('checkbox-catalog');
+                setErrorChkbox('checkbox-testdrive');
+                setErrorChkbox('checkbox-leasing');
+                isValid = false;
+            }
+
+            if ($scope.data.catalog && !$scope.data.catalogTyp) {
+                setErrorRadio('radio-catalog');
+                isValid = false;
+            }
+            if ($scope.data.testdrive && !$scope.data.testdriveTyp) {
+                setErrorRadio('radio-testdrive');
+                isValid = false;
+            }
+            if ($scope.data.leasing && !$scope.data.leasingTyp) {
+                setErrorRadio('radio-leasing');
+                isValid = false;
+            }
+            if (!$scope.data.privacyAccepted) {
+                setErrorChkbox('checkbox-privacy');
+                isValid = false;
+            } else {
+                setNormalChkbox('checkbox-privacy');
+            }
+            return isValid;
+        };
+
+        var validateForm = function () {
+            console.log(!$scope.data.salutation || $scope.data.salutation == '');
+            console.log($scope.data.salutation);
+            if (!$scope.data.salutation || $scope.data.salutation == '') {
+                console.log("asd");
+                $('#salutation_error').removeClass('md-auto-hide');
+                return false;
+            }
+            /*
+             if (!$scope.data.firstName || $scope.data.firstName == '') {
+
+             }
+
+             if(!$scope.data.lastName || $scope.data.lastName == ''){
+             return false;
+             }
+
+             if(!$scope.data.street || $scope.data.street == ''){
+             return false;
+             }
+
+             if(!$scope.data.streetNr || $scope.data.streetNr == ''){
+             return false;
+             }
+
+             if(!$scope.data.zip || $scope.data.zip == ''){
+             return false;
+             }
+
+             if(!$scope.data.city || $scope.data.city == ''){
+             return false;
+             }
+
+             if(!$scope.data.telephone || $scope.data.telephone == ''){
+             return false;
+             }
+
+             var re = '/^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i';
+             if(!$scope.data.email || $scope.data.email == '' || $scope.data.email.match(re)){
+             return false;
+             }*/
+
+            if ($scope.data.catalog) {
+                if (!$scope.data.catalogTyp && $scope.data.catalogTyp == '') {
+                    return false;
+                }
+            }
+
+            if ($scope.data.testdrive) {
+                if (!$scope.data.testdriveTyp || $scope.data.testdriveTyp == '') {
+                    return false;
+                }
+            }
+
+            if ($scope.data.leasing) {
+                if (!$scope.data.leasingTyp || $scope.data.leasingTyp == '') {
+                    return false;
+                }
+            }
+
+            if (!$scope.data.privacyAccepted) {
+                return false;
+            }
+
+            if (!$scope.data.newsletter) {
+                return false;
+            }
+
+            return true;
+        };
+
+        $scope.submit = function () {
+            console.log("ASDASD");
+            if (validateForm()) {
+                console.log('valid');
+                console.log($scope.data);
+            } else {
+                console.log('invalid');
+            }
+        };
+
+        //<-- Event - Listeners -->
+        $scope.$watch('data.privacyAccepted', function (newVal) {
+            if (!newVal && newVal !== '') {
+                setErrorChkbox('checkbox-privacy');
+            } else {
+                setNormalChkbox('checkbox-privacy');
+            }
+        });
+
+        $scope.$watch('data.catalog', function (newVal) {
+            if (!newVal) {
+                $scope.data.catalogTyp = false;
+            }
+            else{
+                setNormalChkbox('checkbox-catalog');
+                setNormalChkbox('checkbox-testdrive');
+                setNormalChkbox('checkbox-leasing');
+            }
+        });
+
+        $scope.$watch('data.testdrive', function (newVal) {
+            if (!newVal) {
+                $scope.data.testdriveTyp = false;
+            }else{
+                setNormalChkbox('checkbox-catalog');
+                setNormalChkbox('checkbox-testdrive');
+                setNormalChkbox('checkbox-leasing');
+            }
+        });
+
+        $scope.$watch('data.leasing', function (newVal) {
+            if (!newVal) {
+                $scope.data.leasingTyp = false;
+            }else{
+                setNormalChkbox('checkbox-catalog');
+                setNormalChkbox('checkbox-testdrive');
+                setNormalChkbox('checkbox-leasing');
+            }
+        });
+
+        $scope.$watch('data.catalogTyp', function (newVal) {
+            if (newVal || $scope.data.catalog) {
+                setNormalRadio('radio-catalog')
+            }else{
+                setErrorRadio('radio-catalog');
+            }
+        });
+        $scope.$watch('data.testdriveTyp', function (newVal) {
+            if (newVal) {
+                setNormalRadio('radio-testdrive')
+            }else{
+                setErrorRadio('radio-testdrive');
+            }
+        });
+        $scope.$watch('data.leasingTyp', function (newVal) {
+            if (newVal) {
+                setNormalRadio('radio-leasing')
+            }else{
+                setErrorRadio('radio-leasing');
+            }
+        });
+
+        //WORKAROUND of Select bug with placeholder translation
+        $rootScope.$on('$translateChangeEnd', function () {
+            $translate('CONTACT_SALUTATION').then(function (trans) {
+                $('#selectSalutation').find('md-select-value').find('span').first().text(trans);
+            });
+        });
+
     }]);
 
 }());
