daeb1a4f4563e6f14fba4c82a7a8357cb2c9da81
duhsokola <duhsokola@gmail.com>
2016-02-25 16:47:53 +0100

    translations EN

--- www/app/components/languageSwitch/languageSwitch.ctrl.js
+++ www/app/components/languageSwitch/languageSwitch.ctrl.js
@@ -11,7 +11,6 @@
             $scope.selectedLanguage = $cookies.get('language');
             $translate.use($scope.selectedLanguage);
         }else{
-            $cookies.put('language',$scope.languages[0]);
             var langExists = false;
             for(var item in $rootScope.global.languages){
                 if(navigator.language.toUpperCase() == $rootScope.global.languages[item]){
@@ -20,6 +19,7 @@
                 }
             }
             $scope.selectedLanguage = langExists ? navigator.language.toUpperCase() : $scope.languages[0];
+            $cookies.put('language',$scope.selectedLanguage);
             $translate.use($scope.selectedLanguage);
         }
 
